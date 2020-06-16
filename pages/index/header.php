39749374997dd8b6ceaf12a10783a96730b34dbf
Unknown <99053034@mydavinci.nl>
2020-06-16 10:43:44 +0200

    B-Little Jewels

--- pages/index/header.php
+++ pages/index/header.php
@@ -12,16 +12,17 @@
 <html lang="nl">
 <head>
 	<link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap&family=Quicksand:wght@300&display=swap" rel="stylesheet">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="assets/css/combi-css.php">
+	<link rel="icon" type="image/vnd.microsoft.icon" href="assets/images/logo/b-littlejewels-logo-32x32.gif">
 	<!--[if lt IE 9]>
 		<link rel='stylesheet' type='text/css' href='assets/css/combi-ie-css.php' />
 	<![endif]-->
 	<title><?php echo $title ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<meta name="application-name" content="">
 	<meta name="web_author" content="">
-	<link rel="icon" type="image/vnd.microsoft.icon" href="">
 </head>
--- pages/index/navbar.php
+++ pages/index/navbar.php
@@ -9,9 +9,11 @@
 
 ?>
 
-
-<div id="companyName">
-	<h2>B-Little Jewels</h2>
+<div id="companyHeader">
+	<a href="home"><img src="assets/images/logo/b-littlejewels-logo-175x175.gif"></a>
+	<div id="companyName">
+		<h2>B-Little Jewels</h2>
+	</div>
 </div>
 <nav>
 	<div id="nav">
@@ -24,7 +26,23 @@
 					<a href="#">Over Ons</a>
 				</div>
 				<div class="navItem">
+					<a href="#">
+						Producten
+						<i class="fas fa-angle-down"></i>
+					</a>
+				</div>
+				<div class="navItem">
 					<a href="#">Contact</a>
+				</div>
+				<div class="navItem">
+					<a href="#">
+						<i class="fas fa-user"></i>
+					</a>
+				</div>
+				<div class="navItem">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
 				</div>
 			</div>
 		</div>
