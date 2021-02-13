4196f2c31b000c7482923bc94d01667c4f9c3a05
Danny <dannyvdweijde@gmail.com>
2021-02-13 16:58:00 +0100

    B-Littlejewels

--- pages/index/header.php
+++ pages/index/header.php
@@ -15,6 +15,15 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="assets/css/combi-css.php">
 	<link rel="icon" type="image/vnd.microsoft.icon" href="assets/images/logo/b-littlejewels-logo-128x128.gif">
+
+		<!-- Verander de favicon naar dit
+		eerste favicon is waarschijnlijk 32x32
+        <link rel='apple-touch-icon-precomposed' href="{{ url('/images/logo-intervisie-favicon.png') }}"/>
+        <link rel='apple-touch-icon-precomposed' sizes='57x57' href="{{ url('/images/logo-intervisie-favicon.png') }}"/>
+        <link rel='apple-touch-icon-precomposed' sizes='72x72' href="{{ url('/images/logo-intervisie-favicon.png') }}"/>
+        <link rel='apple-touch-icon-precomposed' sizes='114x114' href="{{ url('/images/logo-intervisie-favicon.png') }}"/>
+        <link rel='apple-touch-icon-precomposed' sizes='144x144' href="{{ url('/images/logo-intervisie-favicon.png') }}"/> -->
+
 	<!--[if lt IE 9]>
 		<link rel='stylesheet' type='text/css' href='assets/css/combi-ie-css.php' />
 	<![endif]-->
